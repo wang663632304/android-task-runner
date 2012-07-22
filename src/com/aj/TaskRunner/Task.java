4a81f67fcf04e002647f261695feb9a33dedeaec
Arthur <aj@ajinian.com>
2012-07-22 15:59:35 -0700

    Added some documentation for Task.java

--- src/com/aj/TaskRunner/Task.java
+++ src/com/aj/TaskRunner/Task.java
@@ -4,6 +4,43 @@ import android.os.Bundle;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+/**
+ * <p>Base class for all tasks.</p>
+ * <div>All concrete classes that extend this class have to do several things to make this work.</div><br />
+ * <strong>1) A child class must create 2 constructors and call super in both constructors.</strong>
+ * <pre>  
+ * {@code
+ * public MyTask(Bundle params) {
+		super(params);
+	}
+	
+	public MyTask(Parcel in) {
+		super(in);
+	}
+ * }
+ * </pre>
+ * <strong>2) A child class must implement Parcelable.Creator<Task> as a public CREATOR field that generates instances of your Parcelable class from a Parcel. </strong>
+ * <pre> 
+ * {@code
+ * public final static Parcelable.Creator<Task> CREATOR = new Parcelable.Creator<Task>() {
+    	
+        public MyTask createFromParcel(Parcel in) {
+            return new MyTask(in);
+        }
+        public MyTask[] newArray(int size) {
+            return new MyTask[size];
+        }
+    };
+ * }
+ * </pre>
+ *<strong>3) Implement <i>public abstract void execute(TaskProgressListener progressListener, Task task)</i> method with the actual code that has to be executed by this task.</strong>
+ *<pre>
+	public void execute(TaskProgressListener progressListener, Task task) {
+		//Do some lengthy task here....
+	}
+ * </pre>
+ */
+
 public abstract class Task implements Parcelable {
 	
 	public static final String TAG = Task.class.getName();
